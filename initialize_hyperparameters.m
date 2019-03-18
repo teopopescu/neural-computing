f033bf4827502ed50643900acbc8b5b377df1a35
Teodor - Alexandru Popescu <zceitap@ad.ucl.ac.uk>
2019-03-18 20:56:43 +0000

    grid search changes to MLP

---
+++ initialize_hyperparameters.m
@@ -0,0 +1,7 @@
+learning_rate_list=[];
+momentum_list =[];
+number_of_epochs_list=[];
+batch_size_list = [];
+accuracy_list=[];
+cross_entropy_list=[];
+time_list=[];
---
+++ perform_type_conversion.m
@@ -0,0 +1,7 @@
+learning_rate_list=array2table(learning_rate_list);
+momentum_list =array2table(momentum_list);
+number_of_epochs_list=array2table(number_of_epochs_list);
+batch_size_list =array2table(batch_size_list);
+accuracy_list=array2table(accuracy_list);
+cross_entropy_list=array2table(cross_entropy_list);
+time_list=array2table(time_list);
